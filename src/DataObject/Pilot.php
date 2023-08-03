ad71f6219be5d622274f14e4686322877e751fda
William Gomes <mijo.jfwg@gmail.com>
2023-08-03 14:47:33 +0200

    pilot data object added with properties

---
+++ src/DataObject/Pilot.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace William\SevencooksTestTask\DataObject;
+
+class Pilot
+{
+    private string $name;
+    private int $height;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): void
+    {
+        $this->height = $height;
+    }
+}
